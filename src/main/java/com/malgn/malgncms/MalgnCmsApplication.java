1833226f5c170a8364fc10743f37bf47399319f6
kimjaeik95 <wodlrdlsla@naver.com>
2026-03-06 22:51:47 +0900

    feat: 프로젝트 초기 세팅 및 패키지 구조 생성 - 패키지 구조 생성 - application.yml 기본 설정

---
+++ src/main/java/com/malgn/malgncms/MalgnCmsApplication.java
@@ -0,0 +1,13 @@
+package com.malgn.malgncms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalgnCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MalgnCmsApplication.class, args);
+    }
+
+}
